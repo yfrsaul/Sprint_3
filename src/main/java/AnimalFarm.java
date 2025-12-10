17032877371b2337f169264c8d8734cb718fa6c5
Vlad Alekseev <alekseyev.vlad@yandex.ru>
2025-12-10 22:35:42 +0300

    Начал выполнение 3 спринта

---
+++ src/main/java/AnimalFarm.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+public class AnimalFarm {
+    private ArrayList<String> farmAnimals;
+
+    public AnimalFarm(ArrayList<String> farmAnimals) {
+        this.farmAnimals = farmAnimals;
+    }
+
+// Задание 1
+
+    public HashMap<Animal, Integer> countedAnimals() {
+        HashMap<Animal, Integer> animalMap  = new HashMap<>();
+    }
+}
